801a768fa40652fbd9b5c3ebbbb85d0f86e5cad8
Zain <zain.frayha@gmail.com>
2024-05-16 14:17:56 +0200

    added img preview for projects.

--- src/Components/ProjectCard/ProjectCard.jsx
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import githubLogo from "../../assets/Logos/githubLogo.png";
 import toolLogos from "../ToolLogos/toolLogos";
 
+const modalStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    maxWidth: "90%",
+    maxHeight: "90%",
+    position: "relative",
+  },
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.75)",
+  },
+};
+
 const ProjectCard = ({ title, description, imageUrl, link, tools = [] }) => {
-  // console.log("Tool Logos:", toolLogos);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+    document.querySelector("header").classList.add("hidden");
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    document.querySelector("header").classList.remove("hidden");
+  };
 
   return (
     <div className="project-a__card">
-      <img src={imageUrl} alt={title} className="project-a__img" />
+      <img
+        src={imageUrl}
+        alt={title}
+        className="project-a__img"
+        onClick={openModal}
+      />
       <div className="project-a__details">
         <h3 className="project-a__card__h3">{title}</h3>
         <p className="project-a__card__description">{description}</p>
@@ -34,6 +69,22 @@ const ProjectCard = ({ title, description, imageUrl, link, tools = [] }) => {
           Link to Github
         </a>
       </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={modalStyles}
+        contentLabel="Image Preview"
+      >
+        <button onClick={closeModal} className="modal-close-button">
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
+        <img
+          src={imageUrl}
+          alt={title}
+          style={{ width: "100%", height: "100%" }}
+        />
+      </Modal>
     </div>
   );
 };
