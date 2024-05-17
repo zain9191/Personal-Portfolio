5ca710a826b29c9cc2fa90d4cef314ae757ca2f0
Zain <zain.frayha@gmail.com>
2024-05-17 16:52:26 +0200

    added formspree to contact.

--- src/Components/Contcat/contact.jsx
+++ src/Components/Contcat/contact.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 
 const Contact = () => {
+  const [state, handleSubmit] = useForm("xkndyvnb");
+
+  if (state.succeeded) {
+    return <p>Thanks for contacting me!</p>;
+  }
+
   return (
     <section id="contact">
       <div className="container">
@@ -8,21 +15,35 @@ const Contact = () => {
         <p>
           Feel free to reach out to me for any inquiries or just to say hello!
         </p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="name">Name</label>
             <input type="text" id="name" name="name" required />
+            <ValidationError prefix="Name" field="name" errors={state.errors} />
           </div>
           <div className="form-group">
             <label htmlFor="email">Email</label>
             <input type="email" id="email" name="email" required />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="message">Message</label>
             <textarea id="message" name="message" rows="5" required></textarea>
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
           </div>
-          <button type="submit">Send</button>
+          <button type="submit" disabled={state.submitting}>
+            Send
+          </button>
         </form>
+        {state.succeeded && <p>Thanks for contacting me!</p>}
       </div>
     </section>
   );
